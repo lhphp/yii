77b41d997597da7d43f54ab9c81c802b0ecd144e
hui_li <hui_li@allyes.com>
2019-01-27 14:01:20 +0800

    修改1

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,16 +9,16 @@
 <body>
 
 <ul>
-    <li><img src="img/1.jpg"></li>
-    <li><img src="img/2.jpg"></li>
-    <li><img src="img/3.jpg"></li>
-    <li><img src="img/4.jpg"></li>
-    <li><img src="img/5.jpg"></li>
-    <li><img src="img/6.jpg"></li>
-    <li><img src="img/7.jpg"></li>
-    <li><img src="img/8.jpg"></li>
-    <li><img src="img/9.jpg"></li>
-    <li><img src="img/10.jpg"></li>
+    <li><img src="img/1.jpg" width="200" height="200"></li>
+    <li><img src="img/2.jpg" width="200" height="200"></li>
+    <li><img src="img/3.jpg" width="200" height="200"></li>
+    <li><img src="img/4.jpg" width="200" height="200"></li>
+    <li><img src="img/5.jpg" width="200" height="200"></li>
+    <li><img src="img/6.jpg" width="200" height="200"></li>
+    <li><img src="img/7.jpg" width="200" height="200"></li>
+    <li><img src="img/8.jpg" width="200" height="200"></li>
+    <li><img src="img/9.jpg" width="200" height="200"></li>
+    <li><img src="img/10.jpg" width="200" height="200"></li>
 </ul>
 
 </body>
