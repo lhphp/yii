1af17909922d7a579e8a92f3e7eef52998fbf8e6
hui_li <hui_li@allyes.com>
2019-01-29 21:47:26 +0800

    修改

---
+++ console/controllers/test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: longge
+ * Date: 2019/1/9
+ * Time: 1:11 PM
+ */
+
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+
+class TestController extends Controller
+{
+    public function actionIndex(){
+            echo 12321;
+        };
+    
+
+}
